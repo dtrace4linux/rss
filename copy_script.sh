2bcf99998796fa3ea6711799b3043fb69f4b09c4
Paul D Fox <paul.d.fox@gmail.com>
2021-02-19 09:37:20 +0000

    add archive support to copy_script.sh

---
+++ copy_script.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+fn=$1
+
+# Machine to copy to (if it exists)
+dest=berry:release/website/site
+
+# Archive the web pages, hourly.
+mkdir $HOME/.rss/ticker
+t=$(date +%Y%m%d-%H00)
+fn2=$(basename $fn | sed -e "s/.html/-$t.html/")
+cp $fn $HOME/.rss/ticker/$fn2
+
+# echo scp $fn $dest
+scp $fn $dest >/dev/null
